c17f053bc09ea1021f7627184de2bfef64816f7b
andrey100f <andreiforminte19@yahoo.com>
2024-06-04 20:19:23 +0300

    Added discovery server

---
+++ src/test/java/com/ubb/budgetwise_discovery/BudgetwiseDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubb.budgetwise_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetwiseDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
